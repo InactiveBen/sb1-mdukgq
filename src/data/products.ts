65110cb336d4f464556a43c97c80b6fa2e7ab774
InactiveBen <zack@fishbowl.cloud>
2024-11-27 01:12:04 -0600

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,5 +1,17 @@
 import { Product } from '../types';
 
 export const products: Product[] = [
-
+  {
+    id: 'test-product',
+    name: 'Test Product',
+    price: 1,
+    image: 'https://cxpay.events/wp-content/uploads/2024/04/Test-Product-Do-not-Buy.png',
+    rarity: 'normal',
+    category: 'test',
+    stock: 1,
+    featured: true,
+    stripeLink: 'https://buy.stripe.com/example1',
+    description: 'DO NOT BUY THIS ITEM IT IS A TEST ITEM NO ITEM WILL BE SENT',
+    tryOnLink: 'https://www.roblox.com/catalog/'
+  },
 ];
