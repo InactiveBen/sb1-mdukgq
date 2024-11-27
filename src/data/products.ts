d37e58c075dd0ac473c17918e1f6a437eb9d998d
InactiveBen <zack@fishbowl.cloud>
2024-11-27 01:07:57 -0600

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,17 +1,5 @@
 import { Product } from '../types';
 
 export const products: Product[] = [
-  {
-    id: 'robux-listing-4525',
-    name: '100 Robux',
-    price: 1,
-    image: 'https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/original/4X/e/d/f/edfae9388da4cd8496b885a8a2df613372500d9c.png',
-    rarity: 'normal',
-    category: 'robux',
-    stock: 100,
-    featured: true,
-    stripeLink: 'https://buy.stripe.com/example1',
-    description: '1 Robux',
-    tryOnLink: 'https://www.roblox.com/catalog/1048213/BiggerHead'
-  },
+
 ];
