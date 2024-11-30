4b23cdf4fe44badc614a1bcec7e488e2eff3ac16
InactiveBen <zack@fishbowl.cloud>
2024-11-30 17:14:13 -0600

    Update maintenance.ts

--- src/config/maintenance.ts
+++ src/config/maintenance.ts
@@ -1,4 +1,4 @@
 export const maintenanceConfig = {
   enabled: true, // Set to false to disable maintenance mode
-  allowedPaths: ['/success', '/banned', '/support', '/terms', '/privacy', '/maintenance', '/feedback', '/admin/pages/template/request_feedback', '/admin/pages/template/new_login', '/admin/pages/template/seller_signup', '/admin/pages/template/product_sent', '/sitemap']
+  allowedPaths: ['/success', '/gift-cards/success', '/banned', '/support', '/terms', '/privacy', '/maintenance', '/feedback', '/admin/pages/template/request_feedback', '/admin/pages/template/new_login', '/admin/pages/template/seller_signup', '/admin/pages/template/product_sent', '/sitemap']
 };
