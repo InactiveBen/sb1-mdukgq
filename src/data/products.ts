55c8435925ff19ebf47933c3441c0c2c8828860d
InactiveBen <zack@fishbowl.cloud>
2024-12-02 07:20:50 -0600

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,17 +1,22 @@
 import { Product } from '../types';
 
 export const products: Product[] = [
-  {
-    id: 'test-product',
-    name: 'shopblox.gg',
-    price: 1,
-    image: 'https://cxpay.events/wp-content/uploads/2024/04/Test-Product-Do-not-Buy.png',
-    rarity: 'normal',
-    category: 'test',
+   {
+    id: 'rainbow-barf-face',
+    name: 'Rainbow Barf Face',
+    price: 999,
+    robuxPrice: 300000,
+    robuxEnabled: true,
+    robuxLink: 'https://www.roblox.com/game-pass/12345678',
+    image: '/images/4.png',
+    rarity: 'rare',
+    category: 'face',
     stock: 1,
     featured: true,
-    stripeLink: 'https://buy.stripe.com/fZedRB2a547y4esfYZ',
-    description: 'This is a general charge from shopblox.gg.',
-    tryOnLink: 'https://www.roblox.com/catalog/'
+    stripeLink: 'https://buy.stripe.com/example1',
+    placeId: '12345678',
+    description: 'Express yourself with this unique and colorful face accessory that adds a fun, rainbow-themed twist to your avatar!',
+    tryOnLink: 'https://www.roblox.com/catalog/12345678',
+    testLink: 'https://www.roblox.com/games/185655149/Welcome-to-Bloxburg'
   },
 ];
